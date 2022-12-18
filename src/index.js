892c5749f95be6bd2f1457938ed21d0c09d8ce66
tarunmandhan <mandhantarun@gmail.com>
2022-12-19 03:31:06 +0530

    .

--- src/index.js
+++ src/index.js
@@ -3,4 +3,4 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
-ReactDOM.render(document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
