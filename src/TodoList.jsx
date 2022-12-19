ad50bf8ea908304cd4743c0275dd49b411c7736e
tarunmandhan <mandhantarun@gmail.com>
2022-12-19 16:01:15 +0530

    useState & onChange event

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,18 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "@mui/material/Button";
-import AddIcon from "@mui/icons-material/Add"; 
+import AddIcon from "@mui/icons-material/Add";
 
 const TodoList = () => {
+  const [items, setItems] = useState("buy 1kg apple");
+  const itemEvent = (event) => {
+    setItems(event.target.value);
+  };
+
   return (
     <>
       <div className="main_div">
         <div className="center_div">
           <h1>Todo List </h1>
           <br />
-          <input type="text" placeholder="Add Items" />
-          <Button className="btn_new">
+          <input type="text" placeholder="Add Items" onChange={itemEvent} />
+          <Button className="newBtn">
             <AddIcon />
           </Button>
+          <br />
+          <ol>
+            <li>{items}</li>
+          </ol>
+          <br />
         </div>
       </div>
     </>
