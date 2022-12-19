34fed4f5705080951771aebfc24630dbb406289e
tarunmandhan <mandhantarun@gmail.com>
2022-12-19 15:26:31 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import React from "react";
+// import React from "react";
 
-const App = () => {
-  return;
-};
+// const App = () => {
+//   return <></>;
+// };
 
-export default App;
+// export default App;
