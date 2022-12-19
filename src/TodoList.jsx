195fef3aadd2c6fc63e9908053b91243e080742d
tarunmandhan <mandhantarun@gmail.com>
2022-12-19 19:04:14 +0530

    onClick method

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -3,26 +3,39 @@ import Button from "@mui/material/Button";
 import AddIcon from "@mui/icons-material/Add";
 
 const TodoList = () => {
-  const [items, setItems] = useState("");
-  const [newItem, setNewItem] = useState([]); //create new empty array to store input Text
+  const [item, setItem] = useState("");
+  const [newItem, setNewItem] = useState([]); //create new empty array to store input Text get from listOfItems
+
   const itemEvent = (event) => {
-    setItems(event.target.value);
+    setItem(event.target.value);
   };
 
+  const listOfItems = () => {
+    // button click hoga toh kya action hoga is function mien define kiya h. then setNewItem mien store karna h jo updated data hoga newItem ka
+    setNewItem((preValue) => {
+      return [...preValue, item]; //user jo likhega usse is jahag add karna h array ke andar aur previous value bhi
+    });
+    setItem(" ");
+  };
   return (
     <>
       <div className="main_div">
         <div className="center_div">
           <h1>Todo List </h1>
           <br />
-          <input type="text" placeholder="Add Items" onChange={itemEvent} />
-          <Button className="newBtn">
+          <input
+            type="text"
+            value={item}
+            placeholder="Add Items"
+            onChange={itemEvent}
+          />
+          <Button className="newBtn" onClick={listOfItems}>
             <AddIcon />
           </Button>
           <br />
           <ol>
             {newItem.map((val) => {
-              // store input text mapping in newItem
+              // Display input text one by one using mapping method
               return <li>{val}</li>;
             })}
           </ol>
